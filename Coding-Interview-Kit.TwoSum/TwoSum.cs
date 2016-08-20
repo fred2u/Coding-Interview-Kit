f01fa4fc52092103bcfc2e50f15baba2f3dcbf69
fred2u <fd@agilys.be>
2016-08-20 23:02:40 +0200

    Add TwoSum solution

--- Coding-Interview-Kit.TwoSum/TwoSum.cs
+++ Coding-Interview-Kit.TwoSum/TwoSum.cs
@@ -1,17 +1,55 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Coding_Interview_Kit.TwoSum
 {
     public class TwoSum : ITwoSum
     {
-        public void Add(int number)
+
+        private readonly Dictionary<int, int> _numbers;
+
+        public TwoSum()
         {
-            throw new NotImplementedException();
+            _numbers = new Dictionary<int, int>();
         }
 
-        public bool Find(int number)
+        public void Add(int number)
         {
-            throw new NotImplementedException();
+            if (_numbers.ContainsKey(number))
+                _numbers[number]++;
+            else
+                _numbers.Add(number, 1);
         }
+
+        public bool Find(int sum)
+        {
+            IEnumerable<int> numbers = GetNumbersToCheck(sum);
+            foreach (var existingNumber in numbers)
+            {
+                long expectedNumber = (long)sum - existingNumber;
+                if ((int)expectedNumber == expectedNumber)
+                {
+                    if (expectedNumber == existingNumber)
+                    {
+                        if (_numbers[existingNumber] > 1)
+                            return true;
+                    }
+                    else if (_numbers.ContainsKey((int)expectedNumber))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        private IEnumerable<int> GetNumbersToCheck(int sum)
+        {
+            IEnumerable<int> numbers;
+            if (sum < 0)
+                numbers = _numbers.Keys.Where(number => number > sum);
+            else
+                numbers = _numbers.Keys.Where(number => number <= sum);
+            return numbers;
+        }
+
     }
 }
